d642bad722f0ea61441c3636f552b71e5d87278e
Alexis Kirke <47418105+alexiskirke@users.noreply.github.com>
2024-08-01 21:07:05 +0100

    Version 1.0.3 on PyPi. Includes a markdown README
    Version 1.0.3 on PyPi. Includes a markdown README
    Also includes an experimental method 
    def format_screenplay_experimental(data):
            """
            UNDOCUMENTED EXPERIMENTAL METHOD
            Format the screenplay json into a screenplay text.
            Doesn't save result.
            :param data: screenplay json data
            :return: screenplay text
            """

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='screenplay-to-json-openai',
-    version='1.0.0',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -11,8 +11,8 @@ setup(
     author='Alexis Kirke',
     author_email='alexiskirke2@gmail.com',
     description='A tool to a screenplay PDF to JSON format using OpenAI Vision Transformer Analysis.',
-    long_description=open('README.txt').read(),
-    long_description_content_type='text/plain',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
